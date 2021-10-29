5d3d66ae7f40fddde9eb8395fc5467d38dfe5ac7
Ignatov Mike <ruwinmike@gmail.com>
2021-10-29 09:28:20 +0300

    add koin di in iOS app

--- shared/src/commonMain/kotlin/ru/profi/skedda/shared/di/Koin.kt
+++ shared/src/commonMain/kotlin/ru/profi/skedda/shared/di/Koin.kt
@@ -5,11 +5,14 @@ import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.module
 import ru.profi.skedda.shared.network.SkeddaApi
 
-fun initKoin(appDeclaration: KoinAppDeclaration) =
+fun initKoin(appDeclaration: KoinAppDeclaration = {}) =
     startKoin {
         appDeclaration()
         modules(commonModules())
     }
+
+// for iOS
+fun initKoin() = initKoin {}
 
 fun commonModules() = module {
     single { SkeddaApi() }
