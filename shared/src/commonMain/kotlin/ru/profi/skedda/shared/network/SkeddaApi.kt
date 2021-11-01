7db3d252c6c26acf9aa7dd3ded943ec4458de87c
Сергей Демченко <rutelun@ya.ru>
2021-11-01 17:37:43 +0300

    Use verification token (#1)
    Co-authored-by: Сергей Демченко <demchenkoss@eruditor-group.com>

--- shared/src/commonMain/kotlin/ru/profi/skedda/shared/network/SkeddaApi.kt
+++ shared/src/commonMain/kotlin/ru/profi/skedda/shared/network/SkeddaApi.kt
@@ -26,28 +26,44 @@ class SkeddaApi {
     }
 
     @Throws(Exception::class)
-    suspend fun login(email: String, password: String): String {
-        val url = Url(LOGIN_HOST)
+    suspend fun login(email: String, password: String): Unit {
+        val url = Url("${LOGIN_HOST}/logins")
         val loginData = LoginRequestPayload(
             LoginPayload(username = email, password = password)
         )
-        val result = client.post<String>(url) {
+
+        client.post<String>(url) {
             contentType(ContentType.Application.Json)
             body = loginData
         }
-        println(">>> login result $result")
-        return result
+    }
+
+    @Throws(Exception::class)
+    private suspend fun retrieveVerificationToken(): String {
+        val url = Url("${USER_HOST}/booking")
+        val html = client.get<String>(url);
+
+        // FIXME: 01.11.2021 сделать нормальный парсинг
+        val pattern = """<input[^>]+name="__RequestVerificationToken"[^>]+value="(.*?)"""".toRegex()
+
+        // FIXME: 01.11.2021 сделать нормальную ошибку
+        return pattern.find(html)?.destructured?.component1() ?: throw Exception("No request verification token in html");
     }
 
     @Throws(Exception::class)
     suspend fun webs() {
-        val url = Url("https://ruwinmike.skedda.com/webs")
-        val result = client.get<String>(url)
+        val verificationToken = this.retrieveVerificationToken();
+
+        val url = Url("${USER_HOST}/webs")
+        val result = client.get<String>(url) {
+            header("X-Skedda-RequestVerificationToken", verificationToken)
+        }
         println(">>> webs result $result")
     }
 
     internal companion object {
 
-        private const val LOGIN_HOST = "https://app.skedda.com/logins"
+        private const val LOGIN_HOST = "https://app.skedda.com"
+        private const val USER_HOST = "https://ruwinmike.skedda.com"
     }
 }
