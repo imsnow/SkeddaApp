b05cee6e6859d9faa4d75d258d5c12d3c5e7bbac
Ignatov Mike <ruwinmike@gmail.com>
2021-11-01 14:23:32 +0300

    di with viewmodels

--- androidApp/src/main/java/ru/profi/skedda/android/composables/LoginScreen.kt
+++ androidApp/src/main/java/ru/profi/skedda/android/composables/LoginScreen.kt
@@ -2,10 +2,7 @@ package ru.profi.skedda.android.composables
 
 import android.widget.TextView
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -14,7 +11,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import org.koin.androidx.compose.getViewModel
 import ru.profi.skedda.shared.featues.login.LoginViewModel
@@ -24,29 +21,31 @@ fun LoginScreen() {
     val viewModel: LoginViewModel = getViewModel()
     val state = viewModel.state.collectAsState()
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .background(Color.Blue)
+        modifier = Modifier.fillMaxSize()
     ) {
         Text(
-            modifier = Modifier.fillMaxWidth(),
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
             text = "Войдите в свою учётную запись",
             fontSize = 32.sp
         )
         TextField(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth().padding(16.dp),
             value = state.value.email,
             onValueChange = { viewModel.emailChange(it) }
         )
         TextField(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth().padding(16.dp),
             visualTransformation = PasswordVisualTransformation(),
             value = state.value.password,
             onValueChange = { viewModel.onPasswordChanged(it) }
         )
+        Spacer(modifier = Modifier.height(8.dp))
         Button(
+            modifier = Modifier.fillMaxWidth().padding(16.dp),
             enabled = state.value.isActiveButton,
             onClick = { viewModel.login() }
         ) {
--- shared/src/commonMain/kotlin/ru/profi/skedda/shared/di/Koin.kt
+++ shared/src/commonMain/kotlin/ru/profi/skedda/shared/di/Koin.kt
@@ -3,6 +3,7 @@ package ru.profi.skedda.shared.di
 import org.koin.core.context.startKoin
 import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.module
+import ru.profi.skedda.shared.featues.login.LoginViewModel
 import ru.profi.skedda.shared.network.SkeddaApi
 import ru.profi.skedda.shared.validators.EmailValidator
 import ru.profi.skedda.shared.validators.PasswordValidator
@@ -10,7 +11,7 @@ import ru.profi.skedda.shared.validators.PasswordValidator
 fun initKoin(appDeclaration: KoinAppDeclaration = {}) =
     startKoin {
         appDeclaration()
-        modules(commonModules())
+        modules(commonModules(), viewModules())
     }
 
 // for iOS
@@ -20,4 +21,8 @@ fun commonModules() = module {
     single { SkeddaApi() }
     single { EmailValidator }
     single { PasswordValidator }
+}
+
+fun viewModules() = module {
+    factory { LoginViewModel(get(), get(), get()) }
 }
