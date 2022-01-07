c676b28e8de7a60e6324f038ec328c59fe97a73b
Ignatov Mike <ruwinmike@gmail.com>
2022-01-07 22:28:34 +0300

    tag click fix

--- androidApp/src/main/java/ru/profi/skedda/android/composables/ui/Tag.kt
+++ androidApp/src/main/java/ru/profi/skedda/android/composables/ui/Tag.kt
@@ -6,9 +6,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -18,6 +20,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Preview
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun DsTag(
     modifier: Modifier = Modifier,
@@ -26,10 +29,10 @@ fun DsTag(
     onTagClicked: () -> Unit = {}
 ) {
     Surface(
-        modifier = Modifier
-            .clickable { onTagClicked.invoke() },
         shape = RoundedCornerShape(25.dp),
-        color = if (isSelected) ProfiTheme.textBlack else ProfiTheme.buttonGrey
+        onClick = onTagClicked,
+        color = if (isSelected) ProfiTheme.textBlack else ProfiTheme.buttonGrey,
+        indication = rememberRipple()
     ) {
         LRegularText(
             modifier = Modifier.padding(horizontal = 15.dp, vertical = 13.dp),
